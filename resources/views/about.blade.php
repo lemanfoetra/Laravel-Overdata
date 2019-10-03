ce7a5811d45a421da2c8344d1606c1539ac01974
Ardyn Sulaeman <lemanfoetra@gmail.com>
2019-10-03 11:44:46 +0700

    over data

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	Judul {{ $tampil['judul'] }}<br>
+	Tanggal {{ $tampil['tanggal'] }}<br>
+	Array Satu {{ $tampil['dat_arr']['satu'] }}<br>
+	Array dua {{ $tampil['dat_arr']['dua'] }}
+
+</body>
+</html>
